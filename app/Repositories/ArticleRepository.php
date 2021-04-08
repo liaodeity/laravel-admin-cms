2755ae90d5222f459ac34459133de06b78e5d9c4
liaodeity <liaodeity@gmail.com>
2021-04-08 09:25:19 +0800

    添加版权注释

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -1,15 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gui
- * Date: 2020-04-28
- */
+/*
+|-----------------------------------------------------------------------------------------------------------
+| laravel-admin-cms [ 简单高效的开发插件系统 ]
+|-----------------------------------------------------------------------------------------------------------
+| Licensed ( MIT )
+| ----------------------------------------------------------------------------------------------------------
+| Copyright (c) 2020-2021 https://gitee.com/liaodeiy/laravel-admin-cms All rights reserved.
+| ----------------------------------------------------------------------------------------------------------
+| Author: 廖春贵 < liaodeity@gmail.com >
+|-----------------------------------------------------------------------------------------------------------
+*/
 
 namespace App\Repositories;
 
 
 use App\Models\Article;
-use App\Validators\ArticleValidator;
 
 class ArticleRepository extends BaseRepository implements InterfaceRepository
 {
